3ab70a55f37c5f1e584ba97fbf2800d14d65a35b
christoffer-lindow <clindow@gmail.com>
2022-01-30 09:46:29 +0400

    Refactor into pages

---
+++ pages/articles/index.js
@@ -0,0 +1,57 @@
+import { useArticle } from "../../hooks/articles";
+import FetchArticleForm from "../../components/forms/FetchArticleForm/FetchArticleForm";
+import { useState } from "react";
+import { getBoxSizes } from "../api/boxes/sizes";
+import Layout from "../../components/layouts/Layout";
+import HeadSection from "../../components/layouts/HeadSection";
+import Article from "../../components/Article/Article";
+import { getStaticImageUrl } from "../api/article";
+import ArticleNavigation from "../../components/Article/ArticleNavigation";
+
+const Articles = ({ articleImageBaseUrl, boxSizes }) => {
+  const { fetchArticle, updateCurrentArticle } = useArticle();
+  const [article, setArticle] = useState(null);
+  const handleSubmit = async (value) => {
+    await fetchArticle(value)
+      .then((response) => setArticle(response))
+      .catch((error) => console.log(error));
+  };
+
+  const handleUpdateArticle = (article) => {
+    console.log(article);
+    updateCurrentArticle(article);
+    setArticle(article.article);
+  };
+
+  return (
+    <Layout>
+      <HeadSection title={"Party king - Article sizing"} />
+      <FetchArticleForm
+        loading
+        onSubmit={(value) => handleSubmit(value)}
+        currentArticle={article}
+      />
+      <ArticleNavigation
+        updateArticle={(article) => handleUpdateArticle(article)}
+      />
+      {article && (
+        <Article
+          article={article}
+          articleImageBaseUrl={articleImageBaseUrl}
+          boxSizes={boxSizes}
+        />
+      )}
+    </Layout>
+  );
+};
+
+export const getServerSideProps = async () => {
+  return {
+    props: {
+      boxSizes: getBoxSizes(),
+      articleImageBaseUrl: getStaticImageUrl(),
+    },
+  };
+};
+
+export default Articles;
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,12 @@
-import { useArticle } from "../hooks/articles";
-import FetchArticleForm from "../components/forms/FetchArticleForm/FetchArticleForm";
-import { useState } from "react";
-import { getBoxSizes } from "./api/boxes/sizes";
-import Layout from "../components/layouts/Layout";
 import HeadSection from "../components/layouts/HeadSection";
-import Article from "../components/Article/Article";
-import { getStaticImageUrl } from "./api/article";
-import ArticleNavigation from "../components/Article/ArticleNavigation";
+import Layout from "../components/layouts/Layout";
 
-const Home = ({ articleImageBaseUrl, boxSizes }) => {
-  const { fetchArticle, updateCurrentArticle } = useArticle();
-  const [article, setArticle] = useState(null);
-  const handleSubmit = async (value) => {
-    await fetchArticle(value)
-      .then((response) => setArticle(response))
-      .catch((error) => console.log(error));
-  };
-
-  const handleUpdateArticle = (article) => {
-    console.log(article);
-    updateCurrentArticle(article);
-    setArticle(article.article);
-  };
-
+const Home = () => {
   return (
     <Layout>
-      <HeadSection />
-      <FetchArticleForm
-        loading
-        onSubmit={(value) => handleSubmit(value)}
-        currentArticle={article}
-      />
-      <ArticleNavigation
-        updateArticle={(article) => handleUpdateArticle(article)}
-      />
-      {article && (
-        <Article
-          article={article}
-          articleImageBaseUrl={articleImageBaseUrl}
-          boxSizes={boxSizes}
-        />
-      )}
+      <HeadSection title={"Party king - Fighting boredom"} />
     </Layout>
   );
-};
-
-export const getServerSideProps = async () => {
-  return {
-    props: {
-      boxSizes: getBoxSizes(),
-      articleImageBaseUrl: getStaticImageUrl(),
-    },
-  };
 };
 
 export default Home;
